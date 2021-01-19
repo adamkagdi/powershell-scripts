0949246332ed1dc2bbf81b9a4d2ff664318f0123
AdamKagdi <71578844+adamkagdi@users.noreply.github.com>
2021-01-19 14:29:33 +0530

    Adding comments for information on what the command will do

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install chocolatey
-choco install chocolatey-core.extension -y
-choco install notepadplusplus
-choco install vlc
-Invoke-WebRequest -Uri "https://laptop-updates.brave.com/download/CHR253" -OutFile $env:USERPROFILE\Downloads\brave.exe ~/Downloads/brave.exe
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ##This install chocolatey
+choco install chocolatey ## Updates Chocolatey
+choco install chocolatey-core.extension -y ## Downloads few extensions for Chocolatey
+choco install notepadplusplus ## Installs NotePad++
+choco install vlc ## Install Vlc Media Player
+Invoke-WebRequest -Uri "https://laptop-updates.brave.com/download/CHR253" -OutFile $env:USERPROFILE\Downloads\brave.exe ~/Downloads/brave.exe ## Installs the Brave Browser
