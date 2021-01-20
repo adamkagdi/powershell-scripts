28ffd7fc4114366257e0aeff14c7d62171084e0f
AdamKagdi <71578844+adamkagdi@users.noreply.github.com>
2021-01-20 09:47:46 +0530

    Adding the command to install vim and also adding the '-y' flag so chocolatey doesn't ask for confirmation everytime

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ##This install chocolatey
-choco install chocolatey ## Updates Chocolatey
+choco install chocolatey -y ## Updates Chocolatey
 choco install chocolatey-core.extension -y ## Downloads few extensions for Chocolatey
-choco install notepadplusplus ## Installs NotePad++
-choco install vlc ## Install Vlc Media Player
+choco install notepadplusplus -y ## Installs NotePad++
+choco install vlc -y ## Install Vlc Media Player
 Invoke-WebRequest -Uri "https://laptop-updates.brave.com/download/CHR253" -OutFile $env:USERPROFILE\Downloads\brave.exe ~/Downloads/brave.exe ## Installs the Brave Browser
+choco install vim -y ## Install Vim
